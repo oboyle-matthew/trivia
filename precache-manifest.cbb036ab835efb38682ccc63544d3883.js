c2ce78436f2dcd8496e66112c919a70a89c9e794
Matthew O'Boyle <oboyle.matthew@duke.edu>
2020-06-16 23:56:44 +0100

    Updates

---
+++ precache-manifest.cbb036ab835efb38682ccc63544d3883.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea828e48f92c145979dc477359f1748",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ce14366c714b743b852",
+    "url": "/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "cc99502a28d5026eb1f8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4ce14366c714b743b852",
+    "url": "/static/js/2.8e146baa.chunk.js"
+  },
+  {
+    "revision": "22a79e85b7dd2ee15c5a37f1c46ba5ff",
+    "url": "/static/js/2.8e146baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc99502a28d5026eb1f8",
+    "url": "/static/js/main.96dcafb9.chunk.js"
+  },
+  {
+    "revision": "76c2976d75b5061586da",
+    "url": "/static/js/runtime-main.23730869.js"
+  }
+]);
