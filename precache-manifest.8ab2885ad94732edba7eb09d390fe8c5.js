a10fda2660f748dca2d17d4d94273651c9a4ddb9
Matthew O'Boyle <oboyle.matthew@duke.edu>
2020-06-20 19:27:04 +0100

    Updates

---
+++ precache-manifest.8ab2885ad94732edba7eb09d390fe8c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c822e87f6dbc364959593730c8c7c8",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "44ed4cc7b03af3b14124",
+    "url": "/trivia/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "39cbedfc7889237d36a8",
+    "url": "/trivia/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "44ed4cc7b03af3b14124",
+    "url": "/trivia/static/js/2.246003a4.chunk.js"
+  },
+  {
+    "revision": "22a79e85b7dd2ee15c5a37f1c46ba5ff",
+    "url": "/trivia/static/js/2.246003a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39cbedfc7889237d36a8",
+    "url": "/trivia/static/js/main.2c360669.chunk.js"
+  },
+  {
+    "revision": "ef294555bde0349958cc",
+    "url": "/trivia/static/js/runtime-main.36b073f9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c12bdc765bb599a917e3bcc03a4619a4.js"
+  "/trivia/precache-manifest.8ab2885ad94732edba7eb09d390fe8c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trivia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
